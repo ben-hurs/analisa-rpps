f9b0ec9db8eeba8606cef1f6dedda814f9a69fe7
Ben-Hur Santana <besali96@gmail.com>
2023-05-22 11:38:10 -0300

    base pe att

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ import altair as alt
 cad = pd.read_csv('cad/cad_fi.csv',sep = ';', encoding='latin-1')
 cad = cad[['CNPJ_FUNDO', 'DENOM_SOCIAL', 'ADMIN', 'GESTOR', 'TAXA_ADM', 'TAXA_PERFM']]
 
-pe = pd.read_excel('rpps/rpps_pe.xlsx')
+pe = pd.read_excel('rpps/pe_rpps_0323.xlsx')
 rj = pd.read_excel('rpps/rpps_rj.xlsx')
 
 df = pd.concat([pe,rj])
@@ -49,8 +49,14 @@ st.markdown('---')
 
 filtro_municipio = df[df['MUNICÍPIO'] == municipio]
 
+##############################################################################################
+
 lamina = pd.merge(filtro_municipio, cad_cnpj_limpo, how='inner', left_on = 'ID ATIVO', right_on = 'CNPJ_FUNDO')
 lamina = lamina.drop_duplicates('CNPJ_FUNDO').reset_index(drop=True)
+
+nao_lamina = filtro_municipio[~filtro_municipio['ID ATIVO'].isin(lamina['ID ATIVO'])]
+nao_lamina = nao_lamina[['NOME DO FUNDO', 'TIPO DO ATIVO', 'VALOR TOTAL ATUAL', '% DE RECURSOS DO RPPS']].sort_values('TIPO DO ATIVO').reset_index(drop=True)
+
 
 relatorio = lamina[['NOME DO FUNDO', 'GESTOR', 'ADMIN', 'TAXA_ADM', 'TAXA_PERFM', '% DE RECURSOS DO RPPS',
                 'VALOR TOTAL ATUAL','TIPO DO ATIVO']]
@@ -62,32 +68,35 @@ relatorio = lamina[['NOME DO FUNDO', 'GESTOR', 'ADMIN', 'TAXA_ADM', 'TAXA_PERFM'
 #Gráfico de tipo de ativo
 tipo_ativo = relatorio.groupby('TIPO DO ATIVO')['NOME DO FUNDO'].count()
 tipo_ativo = (tipo_ativo/tipo_ativo.sum()*100).round(2)
-#tipo_ativo.columns = [['TIPO DO ATIVO', '%']]
+tipo_ativo = pd.DataFrame(tipo_ativo).reset_index()
+tipo_ativo.columns = ['TIPO DO ATIVO', '%']
 
 
 tipo_ativo = pd.DataFrame(tipo_ativo).reset_index()
 #tipo_ativo
 
 
-graf_pizza = alt.Chart(tipo_ativo).mark_arc(
-    innerRadius=70,
-    outerRadius=100
+graf_pizza = alt.Chart(tipo_ativo).mark_bar(
+    
 ).encode(
-    theta = alt.Theta(field='NOME DO FUNDO', type='quantitative', stack=True),
-    color=alt.Color(
-        field='TIPO DO ATIVO', 
-        type='nominal',
-        legend=None
-    ),
-    tooltip = ['TIPO DO ATIVO', 'NOME DO FUNDO']
-).properties(width=750, height=400)
+    x='%',
+    y='TIPO DO ATIVO',
+    color = 'TIPO DO ATIVO',
+    #color=alt.Color(
+    #    field='TIPO DO ATIVO', 
+    #    #type='nominal',
+    #    legend=None
+    #),
+    tooltip = ['TIPO DO ATIVO', '%'],
+    #alt.Legend(orient='top'),    
+)
 
 rotuloNome = graf_pizza.mark_text(radius=200, size=14).encode(text='TIPO DO ATIVO')
-rotuloValor = graf_pizza.mark_text(radius=165, size=14).encode(text='NOME DO FUNDO')
+rotuloValor = graf_pizza.mark_text(radius=165, size=14).encode(text='%')
 
 
 
-total_investido = relatorio['VALOR TOTAL ATUAL'].sum()
+total_investido = filtro_municipio['VALOR TOTAL ATUAL'].sum()
 taxa_adm = relatorio['TAXA_ADM'].sum()
 taxa_perfm = relatorio['TAXA_PERFM'].sum()
 ###
@@ -95,23 +104,27 @@ col1, col2, col3 = st.columns([0.5,0.5,1])
 
 with col1:
     st.write('**TOTAL INVESTIDO**')
-    st.info(f"R$ {total_investido.round(2)}")
+    st.info(f"R$ {total_investido:,.2f}")
 
     st.write('**TAXA TOTAL DE ADMINISTRAÇÃO**')
-    st.info(f'R$ {taxa_adm}')
+    st.info(f'R$ {taxa_adm:,.2f}')
 
     st.write('**TAXA TOTAL DE PERFORMANCE**')
-    st.info(f'% {taxa_perfm}')
+    st.info(f'{taxa_perfm}%')
 
 
 with col3:
-    st.altair_chart(graf_pizza + rotuloNome +rotuloValor)
+    st.altair_chart(graf_pizza , use_container_width=True)
 
 
 st.markdown('---')
 
 st.subheader('Indicadores para cada fundo na carteira')
 st.dataframe(relatorio, use_container_width=True)
+
+
+st.subheader('Indicadores para tesouro direto e transações bancarias')
+st.dataframe(nao_lamina)
 
 
 
