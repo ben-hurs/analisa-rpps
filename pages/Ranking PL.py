7a85335f997cc61d7111f5df691cd6ab35d79426
Ben-Hur Santana <besali96@gmail.com>
2023-09-19 17:42:35 -0300

    att

--- pages/Ranking PL.py
+++ pages/Ranking PL.py
@@ -29,24 +29,11 @@ pi = pd.read_excel('rpps/pi_rpps_0623.xlsx')
 se = pd.read_excel('rpps/se_rpps_0523.xlsx')
 
 
-pe1 = pd.read_excel('rpps/pe_rpps_0523.xlsx')
-rj1 = pd.read_excel('rpps/rj_rpps_0523.xlsx')
-pb1 = pd.read_excel('rpps/pb_rpps_0523.xlsx')
-al1 = pd.read_excel('rpps/al_rpps_0523.xlsx')
-am1 = pd.read_excel('rpps/am_rpps_0523.xlsx')
-ce1 = pd.read_excel('rpps/ce_rpps_0523.xlsx')
-ma1 = pd.read_excel('rpps/ma_rpps_0523.xlsx')
-pi1 = pd.read_excel('rpps/pi_rpps_0523.xlsx')
-
-#se1 = pd.read_excel('rpps/se_rpps_0523.xlsx')
-#cotas = pd.read_csv('cotas/cotas.csv')
-
 folha_pagamento = pd.read_excel('folha de pagamento/folha_pagamento_pe.xlsx')
 
 
 
-df = pd.concat([pe, rj, pb, al, am, ce, ma, pi,se,
-                pe1, rj1, pb1, al1, am1, ce1, ma1, pi1])
+df = pd.concat([pe, rj, pb, al, am, ce, ma, pi,se])
 
 
 
@@ -58,17 +45,11 @@ with st.sidebar:
 
     # FILTROS ==========================
 
-    mes = st.selectbox(
-        'Seleciona o Mês',
-        df['MÊS'].unique()
-    )
-    filtro_mes = df[df['MÊS'] == mes]
-
     uf = st.selectbox(
     'Selecione o Estado',
-    (filtro_mes['UF']).unique()    
+    (df['UF']).unique()    
     )
-    filtro_uf = filtro_mes[filtro_mes['UF'] == uf]
+    filtro_uf = df[df['UF'] == uf]
 
 
 
