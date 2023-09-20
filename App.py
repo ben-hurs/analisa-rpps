61c6c11cec2492bf8e2713b2af3fc1052bfbe4d9
Ben-Hur Santana <besali96@gmail.com>
2023-09-20 17:27:29 -0300

    att

--- App.py
+++ App.py
@@ -21,33 +21,24 @@ cotas_cvm['Data'] = pd.to_datetime(cotas_cvm['Data'], format = '%Y-%m-%d')
 cad = pd.read_csv('cad/cad_fi.csv',sep = ';', encoding='latin-1')
 cad = cad[['CNPJ_FUNDO', 'DENOM_SOCIAL', 'ADMIN', 'GESTOR', 'TAXA_ADM', 'TAXA_PERFM']]
 
-pasta = r'C:\Users\Ben-Hur\Documents\dashboar streamlit\rpps'
 
-# Lista todos os arquivos na pasta
-arquivos_xlsx = [arquivo for arquivo in os.listdir(pasta) if arquivo.endswith('.xlsx')]
-
-# Crie uma lista para armazenar os DataFrames individuais
-dataframes = []
-
-# Loop pelos arquivos XLSX e leia-os em DataFrames individuais
-for arquivo in arquivos_xlsx:
-    caminho_arquivo = os.path.join(pasta, arquivo)
-    df = pd.read_excel(caminho_arquivo)
-    dataframes.append(df)
-
-# Concatene todos os DataFrames em um único DataFrame
-df = pd.concat(dataframes, ignore_index=True)
-
-
-#pe = pd.read_excel('rpps/pe_rpps_0623.xlsx')
-#rj = pd.read_excel('rpps/rj_rpps_0623.xlsx')
-#pb = pd.read_excel('rpps/pb_rpps_0623.xlsx')
-#al = pd.read_excel('rpps/al_rpps_0623.xlsx')
-#am = pd.read_excel('rpps/am_rpps_0623.xlsx')
-#ce = pd.read_excel('rpps/ce_rpps_0623.xlsx')
-#ma = pd.read_excel('rpps/ma_rpps_0623.xlsx')
-#pi = pd.read_excel('rpps/pi_rpps_0623.xlsx')
-#se = pd.read_excel('rpps/se_rpps_0523.xlsx')
+pe = pd.read_excel('rpps/pe_rpps_0623.xlsx')
+rj = pd.read_excel('rpps/rj_rpps_0623.xlsx')
+pb = pd.read_excel('rpps/pb_rpps_0623.xlsx')
+al = pd.read_excel('rpps/al_rpps_0623.xlsx')
+am = pd.read_excel('rpps/am_rpps_0623.xlsx')
+ce = pd.read_excel('rpps/ce_rpps_0623.xlsx')
+ma = pd.read_excel('rpps/ma_rpps_0623.xlsx')
+pi = pd.read_excel('rpps/pi_rpps_0623.xlsx')
+se = pd.read_excel('rpps/se_rpps_0523.xlsx')
+sc = pd.read_excel('rpps/sc_rpps_0623.xlsx')
+rs = pd.read_excel('rpps/rs_rpps_0623.xlsx')
+pr = pd.read_excel('rpps/pr_rpps_0623.xlsx')
+pa = pd.read_excel('rpps/pa_rpps_0623.xlsx')
+mt = pd.read_excel('rpps/mt_rpps_0623.xlsx')
+ms = pd.read_excel('rpps/ms_rpps_0623.xlsx')
+mg = pd.read_excel('rpps/mg_rpps_0623.xlsx')
+go = pd.read_excel('rpps/go_rpps_0623.xlsx')
 
 
 
@@ -58,7 +49,7 @@ folha_pagamento = pd.read_excel('folha de pagamento/folha_pagamento_pe.xlsx')
 
 
 
-#df = pd.concat([pe, rj, pb, al, am, ce, ma, pi,se])
+df = pd.concat([pe, rj, pb, al, am, ce, ma, pi,se, sc, rs, pr, pa, mt, ms, mg, go])
 
 
 
